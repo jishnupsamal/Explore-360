c85e7ecb6024eaa60cd4436eaa6a54536301f524
Jishnu Prasad Samal <81853902+jishnu-prasad-s@users.noreply.github.com>
2022-08-03 13:45:14 +0000

    feat: added ip tracking

--- src/components/map.js
+++ src/components/map.js
@@ -4,6 +4,7 @@ import { MapContainer } from "react-leaflet/MapContainer"
 import { TileLayer } from "react-leaflet/TileLayer"
 import { useMapEvents, useMap, Marker, Popup } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
+const axios = require("axios").default
 
 function LocationMarker() {
   const [position, setPosition] = useState(null)
@@ -30,21 +31,47 @@ function LocationMarker() {
 const Map = () => {
   const [location, setLocation] = useState("")
   useEffect(() => {
-    fetch("https://ipapi.co/json/?fields=582143").then().catch()
-  })
+    axios
+      .get("https://ipapi.co/json/?fields=582143")
+      .then(res => {
+        console.log(res)
+        setLocation(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  if(!location) return null;
+  
   return (
-    <MapContainer
-      center={{ lat: 51.505, lng: -0.09 }}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: "80vh" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <LocationMarker />
-    </MapContainer>
+    <>
+      <MapContainer
+        center={{ lat: 51.505, lng: -0.09 }}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "80vh" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LocationMarker />
+      </MapContainer>
+
+      <div>
+        <h4>Know Your Locality</h4>
+        <p>
+          <ul>
+            <li>{`Area: ${location.region}, ${location.country_name}`}</li>
+            <li>{`Timezone: ${
+              location.timezone
+            } (UTC${location.utc_offset.slice(0, 3)}:${location.utc_offset.slice(3, 5)})`}</li>
+            <li>{`Currency: ${location.currency_name} (${location.currency})`}</li>
+          </ul>
+        </p>
+      </div>
+    </>
   )
 }
 
