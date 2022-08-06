5c556800a6795829a62162a67531cb67258be2a2
Jishnu Prasad Samal <jishnuprs20@gmail.com>
2022-08-06 15:26:18 +0530

    feat: added world clock

--- src/components/WorldClock.jsx
+++ src/components/WorldClock.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import axios from "axios"
+import Moment from "react-moment"
+import "moment-timezone"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Card from "react-bootstrap/Card"
 
 const WorldClock = ({}) => {
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://ipapi.co/json/?fields=582143")
+      .then(res => {
+        setData(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  console.log(data.utc_offset)
+  console.log(data.country_name)
   return (
     <>
-        World Clock
+      <Card border="success" className="text-center d-flex justify-content-center">
+        <Card.Body>
+          <Card.Title width="3rem" as="h2">
+            <Moment
+              format="hh [:] mm [:] ss a"
+              interval={1000}
+              tz={`${data.utc_offset}`}
+            />
+          </Card.Title>
+        </Card.Body>
+        <Card.Footer>{data.country_name}</Card.Footer>
+      </Card>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,13 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-import Header from "../components/header";
+import WorldClock from "../components/WorldClock";
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Explore 360</h1>
+    <WorldClock />
   </Layout>
 )
 
