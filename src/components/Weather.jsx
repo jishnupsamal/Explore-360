719ff13d9cca643e6f0241d6a98268deb0db8e37
Jishnu Prasad Samal <jishnuprs20@gmail.com>
2022-08-07 19:29:43 +0530

    improv: improved weather widget

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import Card from "react-bootstrap/Card"
+import Spinner from 'react-bootstrap/Spinner';
 import { MdLocationPin, MdVisibility } from "react-icons/md"
 import { FaTemperatureLow } from "react-icons/fa"
-import { BsWind, BsSunsetFill, BsSunriseFill } from "react-icons/bs"
+import { BsWind, BsCloudsFill } from "react-icons/bs"
 
 const Weather = () => {
   // const [location, setLocation] = useState({})
@@ -13,9 +14,13 @@ const Weather = () => {
   useEffect(() => {
     axios
       .get("http://ip-api.com/json/?fields=42004479")
-      .then(res => {return res.data})
+      .then(res => {
+        return res.data
+      })
       .then(location => {
-        return axios.get(`https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${location.lat}&lon=${location.lon}&appid=${process.env.GATSBY_OPENWEATHER_API_KEY}`)
+        return axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${location.lat}&lon=${location.lon}&appid=${process.env.GATSBY_OPENWEATHER_API_KEY}`
+        )
       })
       .then(res => {
         return setWeather(res.data)
@@ -48,48 +53,52 @@ const Weather = () => {
 
   return (
     <>
-    {weather !== null && (
       <Card
         border="primary"
         className="text-center d-flex justify-content-center bg-warning text-light my-4"
       >
-        <Card.Body>
-          {/* <Card.Title width="3rem" as="h2">
+        {weather !== null ? (
+          <Card.Body>
+            {/* <Card.Title width="3rem" as="h2">
             
           </Card.Title> */}
-          <ul style={{ listStyleType: "none" }}>
-            <li>
-              <img
-                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt={weather.weather[0].main}
-                width="75"
-                height="75"
-              />
-            </li>
-            <li>
-              <MdLocationPin /> {weather.name}
-            </li>
-            <li>
-              <FaTemperatureLow /> {weather.main.temp} &#176;C
-            </li>
-            {/* <li>
+            <ul style={{ listStyleType: "none" }}>
+              <li>
+                <img
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt={weather.weather[0].main}
+                  width="75"
+                  height="75"
+                />{" "}
+                {weather.weather[0].main}
+              </li>
+              <li>
+                <MdLocationPin /> {weather.name}
+              </li>
+              <li>
+                <FaTemperatureLow /> {weather.main.temp} &#176;C
+              </li>
+              {/* <li>
               <BsSunriseFill /> <BsSunsetFill />
             </li> */}
-            <li>
-              <BsWind /> {weather.wind.speed} m/s{" "}
-            </li>
-            <li>Cloudiness: {weather.clouds.all}%</li>
-            <li>
-              <MdVisibility />{" "} {weather.visibility > 1000 ? (
-                `${(weather.visibility)/1000} km`
-              ):(
-                `${weather.visibility} m`
-              )}
-            </li>
-          </ul>
-        </Card.Body>
+              <li>
+                <BsWind /> {weather.wind.speed} m/s{" "}
+              </li>
+              <li>
+                <BsCloudsFill /> {weather.clouds.all}%
+              </li>
+              <li>
+                <MdVisibility />{" "}
+                {weather.visibility > 1000
+                  ? `${weather.visibility / 1000} km`
+                  : `${weather.visibility} m`}
+              </li>
+            </ul>
+          </Card.Body>
+        ) : (
+          <Spinner animation="grow" variant="success" />
+        )}
       </Card>
-      )}
     </>
   )
 }
